5dde957a0a6c7cd2f3b311c8551663deeb325577
大城　慶知 <e165745@ie.u-ryukyu.ac.jp>
2016-11-21 18:20:10 +0900

    Programing Language Report4: Get use unit tests and version management by refactering

--- Report4/src/main/jp/ac/uryukyu/ie/e165745/Main.java
+++ Report4/src/main/jp/ac/uryukyu/ie/e165745/Main.java
@@ -15,12 +15,7 @@ public class Main {
             turn++;
             System.out.printf("%dターン目開始！\n", turn);
             hero.attack(enemy);
-            if( enemy.isDead() == true){
-                break;
-            }
-            else{
             enemy.attack(hero);
-            }
         }
         System.out.println("戦闘終了");
     }
